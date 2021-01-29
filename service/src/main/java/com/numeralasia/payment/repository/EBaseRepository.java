aa3de8ce4a8da18f2eca19b05c0b77a6ff128043
Alfarabi Dwi Karuniawan <alfarabi@Alfarabis-MacBook-Pro.local>
2021-01-29 18:57:16 +0700

    -

--- service/src/main/java/com/numeralasia/payment/repository/EBaseRepository.java
+++ service/src/main/java/com/numeralasia/payment/repository/EBaseRepository.java
@@ -3,9 +3,11 @@ package com.numeralasia.payment.repository;
 import com.numeralasia.payment.entity.EBase;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
 
+@NoRepositoryBean
 public interface EBaseRepository<T extends EBase> extends JpaRepository<T, Long> {
 
     List<T> findByActive(Boolean active);
