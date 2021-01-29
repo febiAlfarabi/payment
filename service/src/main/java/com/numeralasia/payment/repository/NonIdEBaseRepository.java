08c123b995c59d16d71711ed1d0984c3a6236d43
Alfarabi Dwi Karuniawan <alfarabi@Alfarabis-MacBook-Pro.local>
2021-01-29 18:57:45 +0700

    -

--- service/src/main/java/com/numeralasia/payment/repository/NonIdEBaseRepository.java
+++ service/src/main/java/com/numeralasia/payment/repository/NonIdEBaseRepository.java
@@ -3,9 +3,11 @@ package com.numeralasia.payment.repository;
 import com.numeralasia.payment.entity.NonIdEBase;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
 
+@NoRepositoryBean
 public interface NonIdEBaseRepository<T extends NonIdEBase> extends JpaRepository<T, Long> {
 
     List<T> findByActive(Boolean active);
