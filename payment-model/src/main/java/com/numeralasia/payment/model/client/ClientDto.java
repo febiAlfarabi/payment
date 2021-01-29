96cad89936e21676992540093c4b95ef4168f0f3
Alfarabi Dwi Karuniawan <alfarabi@Alfarabis-MacBook-Pro.local>
2021-01-29 10:24:57 +0700

    -

--- payment-model/src/main/java/com/numeralasia/payment/model/client/ClientDto.java
+++ payment-model/src/main/java/com/numeralasia/payment/model/client/ClientDto.java
@@ -1,4 +1,15 @@
 package com.numeralasia.payment.model.client;
 
+import lombok.Data;
+
+@Data
 public class ClientDto {
+
+    String name ;
+    String reference ;
+    String serviceAddress ;
+    String companyClass ;
+    String domainClass ;
+    String notificationEndpoint ;
+
 }
---
+++ payment-model/src/main/java/com/numeralasia/payment/model/midtrans/TransactionGateDto.java
@@ -0,0 +1,18 @@
+package com.numeralasia.payment.model.midtrans;
+
+import com.numeralasia.payment.model.EBaseDto;
+import com.numeralasia.payment.model.client.ClientDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@ToString(of = "id")
+@EqualsAndHashCode(callSuper = true, of = "id")
+public class TransactionGateDto extends EBaseDto {
+
+    String orderId ;
+
+    ClientDto client ;
+
+}
