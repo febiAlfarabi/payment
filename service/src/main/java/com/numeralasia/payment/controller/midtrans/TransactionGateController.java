f97da1ae05a573168ae5cc7e2559e24e30d6777a
Alfarabi Dwi Karuniawan <alfarabi@Alfarabis-MacBook-Pro.local>
2021-01-31 03:24:46 +0700

    -

--- service/src/main/java/com/numeralasia/payment/controller/midtrans/TransactionGateController.java
+++ service/src/main/java/com/numeralasia/payment/controller/midtrans/TransactionGateController.java
@@ -42,7 +42,7 @@ public class TransactionGateController extends BasicController {
     @PostMapping(path = "/snap/v1/transactions",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<MidtransChargeResponse> notification(@RequestHeader(Constant.REFERENCE) String referenceBase64,
+    public ResponseEntity<MidtransChargeResponse> notification(@RequestParam(Constant.REFERENCE) String referenceBase64,
                                                                HttpServletRequest request) throws Exception {
         String reference = new String(Base64.decode(referenceBase64, Base64.NO_WRAP));
         String body = IOUtils.toString(request.getReader());
