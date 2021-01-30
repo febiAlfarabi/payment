38ada136c96297daaebecb888d2963f42aa31377
Alfarabi Dwi Karuniawan <alfarabi@Alfarabis-MacBook-Pro.local>
2021-01-31 00:16:36 +0700

    -

--- service/src/main/java/com/numeralasia/payment/PaymentServiceApplication.java
+++ service/src/main/java/com/numeralasia/payment/PaymentServiceApplication.java
@@ -1,11 +1,14 @@
 package com.numeralasia.payment;
 
+import com.numeralasia.payment.component.BodyReaderFilter;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
@@ -30,6 +33,15 @@ public class PaymentServiceApplication {
 
         thread.setDaemon(false);
         thread.start();
+    }
+
+    @Bean
+    public FilterRegistrationBean<BodyReaderFilter> Filters() {
+        FilterRegistrationBean<BodyReaderFilter> registrationBean = new FilterRegistrationBean<BodyReaderFilter>();
+        registrationBean.setFilter(new BodyReaderFilter());
+        registrationBean.addUrlPatterns("/**");
+        registrationBean.setName("bodyReaderFilter");
+        return registrationBean;
     }
 
 }
