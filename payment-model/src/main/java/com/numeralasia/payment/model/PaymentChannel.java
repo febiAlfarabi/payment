1f3e40405204f2a8a281927864e99106f521d2ab
Alfarabi Dwi Karuniawan <alfarabi@10.0.1.195>
2021-02-01 15:19:23 +0700

    -

--- payment-model/src/main/java/com/numeralasia/payment/model/PaymentChannel.java
+++ payment-model/src/main/java/com/numeralasia/payment/model/PaymentChannel.java
@@ -2,5 +2,6 @@ package com.numeralasia.payment.model;
 
 public class PaymentChannel {
     public static final String MIDTRANS = "MIDTRANS";
+    public static final String DEBIT_BALANCE = "DEBIT_BALANCE";
 
 }
