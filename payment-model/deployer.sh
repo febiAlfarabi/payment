bf366f54fd54c82d8b91e200ecaedce506fbab13
Alfarabi Dwi Karuniawan <numeralasia@gmail.com>
2021-10-12 16:02:08 +0700

    -

--- payment-model/deployer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-(git add --all && git commit -m "-" && git pull origin master && git push origin master ) &&
-mvn clean install -U && mvn release:prepare -Dresume=false && mvn release:perform -X
-exit 0
